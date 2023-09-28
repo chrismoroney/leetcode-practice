fb4d53e8c47aea7585b559eca2300db6c4d2893f
chrismoroney <christopher.moroney1@gmail.com>
2023-09-27 21:37:35 -0700

    full solution

--- ValidSudoku/src/Solution.java
+++ ValidSudoku/src/Solution.java
@@ -2,25 +2,14 @@ import java.util.HashMap;
 
 public class Solution {
     public boolean isValidSudoku(char[][] board) {
-        return isRowValid(board) && isColumnValid(board);
+        return isRowValid(board) && isColumnValid(board) && isSquaresValid(board);
     }
 
     private boolean isRowValid(char[][] board) {
+
         for (char[] row : board) {
-            HashMap<Character, Integer> map = new HashMap<>();
-            for (char element : row){
-                if (map.containsKey(element)){
-                    int count = map.get(element);
-                    map.put(element, count + 1);
-                } else {
-                    map.put(element, 1);
-                }
-            }
-            for (Character key : map.keySet()) {
-                Integer value = map.get(key);
-                if (key != '.' && value > 1){
-                    return false;
-                }
+            if (!checkInvalid(row)){
+                return false;
             }
         }
         return true;
@@ -34,25 +23,53 @@ public class Solution {
                 columnVals[row] = board[row][column];
             }
 
-            HashMap<Character, Integer> map = new HashMap<>();
-            for (char element : columnVals){
-                if (map.containsKey(element)){
+            if (!checkInvalid(columnVals)){
+                return false;
+            }
+
+        }
+
+        return true;
+    }
+
+    private boolean isSquaresValid(char[][] board) {
+        for (int squareRow = 0; squareRow < 3; squareRow++) {
+            for (int squareCol = 0; squareCol < 3; squareCol++) {
+
+                char[] squareVals = new char[9];
+                int index = 0;
+                for (int i = 0; i < 3; i++) {
+                    for (int j = 0; j < 3; j++) {
+                        squareVals[index++] = board[squareRow * 3 + i][squareCol * 3 + j];
+                    }
+                }
+
+                if (!checkInvalid(squareVals)){
+                    return false;
+                }
+
+            }
+        }
+        return true;
+    }
+
+    private boolean checkInvalid(char[] arr){
+
+        HashMap<Character, Integer> map = new HashMap<>();
+        for (char element : arr){
+            if (map.containsKey(element)){
                     int count = map.get(element);
                     map.put(element, count + 1);
                 } else {
                     map.put(element, 1);
                 }
             }
-
             for (Character key : map.keySet()) {
                 Integer value = map.get(key);
                 if (key != '.' && value > 1){
                     return false;
                 }
             }
-            
-        }
-
         return true;
     }
 }
