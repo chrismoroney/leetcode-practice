e3981b0f40069f174def581fa863c76dc1b72b5c
chrismoroney <christopher.moroney1@gmail.com>
2023-09-27 21:18:49 -0700

    add column checking

--- ValidSudoku/src/Solution.java
+++ ValidSudoku/src/Solution.java
@@ -2,7 +2,7 @@ import java.util.HashMap;
 
 public class Solution {
     public boolean isValidSudoku(char[][] board) {
-        return isRowValid(board);
+        return isRowValid(board) && isColumnValid(board);
     }
 
     private boolean isRowValid(char[][] board) {
@@ -24,6 +24,35 @@ public class Solution {
             }
         }
         return true;
-          
+    }
+
+    private boolean isColumnValid(char[][] board) {
+
+        for (int column = 0; column < board.length; column++) {
+            char[] columnVals = new char[9];
+            for (int row = 0; row < board.length; row++) {
+                columnVals[row] = board[row][column];
+            }
+
+            HashMap<Character, Integer> map = new HashMap<>();
+            for (char element : columnVals){
+                if (map.containsKey(element)){
+                    int count = map.get(element);
+                    map.put(element, count + 1);
+                } else {
+                    map.put(element, 1);
+                }
+            }
+
+            for (Character key : map.keySet()) {
+                Integer value = map.get(key);
+                if (key != '.' && value > 1){
+                    return false;
+                }
+            }
+            
+        }
+
+        return true;
     }
 }
