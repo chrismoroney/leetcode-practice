aca263e00dcc1299d7d832374db2b033defed7ff
Chris <christopher.moroney1@gmail.com>
2024-11-25 15:30:55 -0800

    Create fizz_buzz.py

---
+++ Arrays and Strings/fizz_buzz.py
@@ -0,0 +1,34 @@
+# Fizz Buzz
+
+# Given an integer n, return a string array answer (1-indexed) where:
+
+# answer[i] == "FizzBuzz" if i is divisible by 3 and 5.
+# answer[i] == "Fizz" if i is divisible by 3.
+# answer[i] == "Buzz" if i is divisible by 5.
+# answer[i] == i (as a string) if none of the above conditions are true.
+
+class Solution(object):
+    def fizz_buzz(self, n):
+        """
+        :type n: int
+        :rtype: List[str]
+        """
+        return_arr = [""] * n
+
+        for i in range(1, n + 1):
+            if i % 3 == 0 and i % 5 == 0:
+                return_arr[i-1] = "FizzBuzz"
+            elif i % 3 == 0 and not i % 5 == 0:
+                return_arr[i-1] = "Fizz"
+            elif not i % 3 == 0 and i % 5 == 0:
+                return_arr[i-1] = "Buzz"
+            else:
+                return_arr[i-1] = str(i)
+        
+        return return_arr
+    
+if __name__ == "__main__":
+    n = 53
+    sol = Solution()
+    print(sol.fizz_buzz(n))
+        
