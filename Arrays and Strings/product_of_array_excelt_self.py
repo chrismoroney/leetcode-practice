6145f17f12908f99aaabb7716cdddc3c4af50a3e
Christopher Moroney <christopher.moroney1@gmail.com>
2025-02-21 11:17:52 -0800

    Update product_of_array_excelt_self.py

--- Arrays and Strings/product_of_array_excelt_self.py
+++ Arrays and Strings/product_of_array_excelt_self.py
@@ -6,6 +6,8 @@ Given an integer array nums, return an array answer such that answer[i] is equal
 The product of any prefix or suffix of nums is guaranteed to fit in a 32-bit integer.
 
 You must write an algorithm that runs in O(n) time and without using the division operation.
+
+Follow up: Can you solve the problem in O(1) extra space complexity? (The output array does not count as extra space for space complexity analysis.)
 """
 from typing import List
 
